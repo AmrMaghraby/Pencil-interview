228295e87bc23cbe0e6ae1eff4f30c932a2216d1
AmrMaghraby <amr.alkeleay@gmail.com>
2021-02-10 16:11:30 +0200

    Implemnted frontend task

---
+++ PencilFrontend/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA5wNzOdFdBYKioG8O_wIuai-PcW94sacU",
+    authDomain: "pencil-78fc9.firebaseapp.com",
+    databaseURL: "https://pencil-78fc9-default-rtdb.firebaseio.com",
+    projectId: "pencil-78fc9",
+    storageBucket: "pencil-78fc9.appspot.com",
+    messagingSenderId: "780284240450",
+    appId: "1:780284240450:web:1c54e6123b6188e2f583db",
+    measurementId: "G-21SLR3ZW1R"
+  }
+};
